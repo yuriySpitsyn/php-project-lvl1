73f4a71c40ccc387bb896c87aac4b5c749e96656
phpshka <jur.spitsyn@gmail.com>
2019-11-20 12:00:10 +0300

    fix errors lint

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace BrainGames\Cli;
 
-use function \cli\line;
-use function \cli\prompt;
+use function cli\line;
+use function cli\prompt;
 
 function run()
 {
-line('Welcome to the Brain Games!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
